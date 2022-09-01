2f45916a85c9502fa04be6fdb80b368df2891313
Tyler Fabian <tylerf808@gmail.com>
2022-09-01 12:46:52 -0400

    Uploading test button

--- chop-shop-app/src/App.js
+++ chop-shop-app/src/App.js
@@ -8,111 +8,46 @@ import FreshSausage from "./components/FreshSausage";
 import SnackSticks from "./components/SnackSticks";
 import TotalCalculator from "./components/TotalCalculator";
 import { Container } from "@mui/system";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Typography } from "@mui/material";
-import { useState } from "react";
+import { ThemeProvider, createTheme, useTheme } from '@mui/material/styles';
+import { Button, Typography } from "@mui/material";
+import { useState, createContext, useMemo } from "react";
 
 function App() {
 
   const theme = createTheme({
     palette: {
-      primary: {
-        main: '#4A79DE'
-      },
-      secondary: {
-        main: '#809AAF'
-      },
-    },
-  })
-
-  const [receipt, changeReceipt] = useState({
-    deerNum: 0,
-    confNum: 0,
-    name: '',
-    PH: 0,
-    dropDate: 0,
-    lbs: 0,
-    buckOrDoe: '',
-    deboneQts: 0,
-    skinForMount: '',
-    packs: {
-      tenderloin: 0,
-      roast: 0,
-      steak: 0,
-      fish: 0,
-      minuteSteak: 0,
-      burgers: {
-        plain: 0,
-        addPork: 0,
-        addBeef: 0
-      }
-    },
-    jerky: 0,
-    pastrami: 0,
-    chipHam: 0,
-    bologna: {
-      pep: 0,
-      pepCheese: 0,
-      sweet: 0,
-      sweetCheese: 0,
-    },
-    HotDogs: {
-      halfSmoke: 0,
-      plain: 0,
-      jalCheese: 0,
-      plainCheese: 0,
-    },
-    snackStick: {
-      pepPlain: 0,
-      pepCheese: 0,
-      honeyPlain: 0,
-      honeyCheese: 0
-    },
-    freshSausage: {
-      break: 0,
-      sweetItal: 0,
-      hotItal: 0,
-      smokeBaconBurg: 0
+      mode: 'light'
     }
   })
-  const [total, changeTotal] = useState({
-    total: 0,
-  })
 
-  const addToTotal = (e) => {
-    const num = e.target.value;
-    const oldNum = total.total;
-    const newNum = num + oldNum;
-    const newTotal = {
-      total: newNum
-    }
-    changeTotal(newTotal)
-  }
+  const [receipt, changeReceipt] = useState([{
+    subTotal: 0,
+  }])
 
   return (
     <ThemeProvider theme={theme}>
-      <Container className="App" maxWidth="lg">
-        <Typography variant="h3">
+      <Container className="App" maxWidth="lg" >
+        <Typography variant="h3" >
           The Chop Shop
         </Typography>
-        <Typography variant="h4">
+        <Typography variant="h4" >
           6625 Taneytown Pike, Taneytown, MD 21787
         </Typography>
-        <Typography variant="h4">
+        <Typography variant="h4" >
           443-415-4643
         </Typography>
-        <DeerInfo onChange={addToTotal} />
-        <DeerOptions onChange={addToTotal} />
+        <DeerInfo />
+        <DeerOptions />
         <MeatType />
         <Jerky />
         <Bologna />
         <HotDogs />
         <SnackSticks />
         <FreshSausage />
-        <TotalCalculator total={total.total}></TotalCalculator>
+        {receipt.subTotal}
       </Container>
     </ThemeProvider>
   );
 }
 
-export default App;
+export default App
--- chop-shop-app/src/components/DeerInfo.jsx
+++ chop-shop-app/src/components/DeerInfo.jsx
@@ -3,20 +3,24 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import { Input, Typography } from '@mui/material';
+import { Button, Input, Typography } from '@mui/material';
 
-function DeerInfo({onChange}) {
+function DeerInfo(props) {
+
+    const addSubtotal = () => {
+
+    }
 
     return (
         <Container className="deer-info-container" sx={{ border: 2, borderRadius: 2, display: 'flex', 
         flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap' }}>
             <FormControl sx={{margin: 3}}>
                 <Typography variant='body1'>Deer #:</Typography>
-                <Input onChange={onChange} id='test' type='number'></Input>
+                <Input id='deer-num' type='number'></Input>
             </FormControl>
             <FormControl sx={{margin: 3}}>
                 <Typography variant='body1'>Conf #:</Typography>
-                <Input type='number'></Input>
+                <Input min={0} type='number'></Input>
             </FormControl>
             <FormControl sx={{margin: 3}}>
                 <Typography variant='body1'>Name</Typography>
@@ -40,6 +44,7 @@ function DeerInfo({onChange}) {
                     <FormControlLabel value="doe" control={<Radio />} label="Doe" />
                 </RadioGroup>
             </FormControl>
+            <Button variant="contained" color="success" onClick={addSubtotal}>Update Total</Button>
         </Container >
     );
 }
